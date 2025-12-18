f50f15d679e03c612545fee2a385dfeb93f19e5d
Narendra Mahalle <narendramahalle4655@gmail.com>
2025-12-18 18:59:06 +0530

    Add User Management project

---
+++ React/UserManagement/src/features/users/constants/user.constants.js
@@ -0,0 +1,11 @@
+export const USER_ROLES = {
+  ADMIN: "Admin",
+  MANAGER: "Manager",
+  USER: "User"
+};
+
+export const USER_STATUS = {
+  ACTIVE: "Active",
+  INACTIVE: "Inactive",
+  SUSPENDED: "Suspended"
+};
---
+++ React/UserManagement/src/features/users/services/user.service.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    id: 1,
+    name: "Narendra Mahalle",
+    email: "narendra@gmail.com",
+    role: "Admin",
+    status: "Active"
+  },
+  {
+    id: 2,
+    name: "Rahul Thatar",
+    email: "rahulT@gmail.com",
+    role: "User",
+    status: "Inactive"
+  }
+];
